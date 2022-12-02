ae33af888d377cbd511c92453081c8dae5873976
pasca <pascal>
2022-12-02 14:35:30 +0100

    Suppression d'un truc inutile

--- src/Entity/Advert.php
+++ src/Entity/Advert.php
@@ -6,7 +6,6 @@ use ApiPlatform\Doctrine\Orm\Filter\OrderFilter;
 use ApiPlatform\Doctrine\Orm\Filter\RangeFilter;
 use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
 use ApiPlatform\Metadata\ApiFilter;
-use ApiPlatform\Metadata\ApiProperty;
 use ApiPlatform\Metadata\ApiResource;
 use App\Repository\AdvertRepository;
 use Doctrine\DBAL\Types\Types;
